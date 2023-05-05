6338efb98fea7e335602f4b4e586703847ae7ebb
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-05-05 22:44:52 +0100

    added event listener

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const pounds = document.querySelector(".pounds");
 const kilograms = document.querySelector(".kilograms");
 const ounces = document.querySelector(".ounces");
 const grams = document.querySelector(".grams");
+const form = document.querySelector("form");
+
+form.addEventListener("input", convertWeight)
