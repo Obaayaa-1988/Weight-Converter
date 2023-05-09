e3a6d1555cbeff617eb2f96bc8ab34ed1e7a9f8b
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-05-09 23:28:23 +0100

    fixed function error

--- script.js
+++ script.js
@@ -6,31 +6,29 @@ const form = document.querySelector("form");
 
 form.addEventListener("input", convertWeight)
 
-function convertWeight(){
+function convertWeight(e){
     if(e.target.classlist.contains("pounds")){
         let x = e.target.value;
         kilograms.value = (x / 2.2046).toFixed(2);
         grams.value = (x / 0.0022046).toFixed(2);
         ounces.value = (x * 16).toFixed(2);
     }
-}
-function convertWeight(){
+
     if(e.target.classlist.contains("kilograms")){
         let x = e.target.value;
         pounds.value = x * 2.2046;
         grams.value = x * 1000;
         ounces.value = x * 35.274;
     }
-}
-function convertWeight(){
+
+
     if(e.target.classlist.contains("grams")){
         let x = e.target.value;
         pounds.value = x * 0.0022046;
         kilograms.value = x / 1000;
         ounces.value = x * 0.035274;
     }
-}
-function convertWeight(){
+
     if(e.target.classlist.contains("ounces")){
         let x = e.target.value;
         pounds = x * 0.0625;
